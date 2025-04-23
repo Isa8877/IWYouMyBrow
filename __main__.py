617b3bb4636538079b6fc6c89117bd7918ec239e
GonzaTools Systems <122550583+Isa8877@users.noreply.github.com>
2025-04-23 01:27:01 -0500

    Crear __main__.py

---
+++ __main__.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3 
+
+"""
+IWYouMyBrow entrypoint
+"""
+
+import asyncio 
+
+from IWYouMyBrow import main 
+    
+if __name__ == '__main__.py':
+    asyncio.run(main())
+    
+    
+    
